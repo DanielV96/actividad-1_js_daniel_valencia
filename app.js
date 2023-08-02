ceef4a43533c45383edc32c108a54d28b40d39b7
DanielV96 <el.dani72@gmail.com>
2023-08-01 20:01:18 -0500

    Entregable clase 9

---
+++ app.js
@@ -0,0 +1,18 @@
+let name = 'Daniel'
+let lastName = 'Valencia'
+let age = 27
+const birthDate = '1996/03/21'
+let city = 'Medellín'
+
+console.log(
+  'My name is',
+  name,
+  lastName,
+  'I am',
+  age,
+  'years old',
+  'I was born in',
+  birthDate,
+  'and I live in   ',
+  city
+)
